59c9b5fe8db5eadce0f9dddf3957b47db4aaf4df
微博@裕之都 <1632212092@qq.com>
2016-12-26 16:48:38 +0800

    增加头文件

---
+++ MBProgressHUDExtendDemo/MBProgressHUDExtend/MBProgressHUDExtend.h
@@ -0,0 +1,14 @@
+//
+//  MBProgressHUDExtend.h
+//  MBProgressHUDExtendDemo
+//
+//  Created by 马裕博 on 2016/12/26.
+//  Copyright © 2016年 yuzhidu. All rights reserved.
+//
+
+#ifndef MBProgressHUDExtend_h
+#define MBProgressHUDExtend_h
+
+#import "MBProgressHUD+YU.h"
+
+#endif /* MBProgressHUDExtend_h */
